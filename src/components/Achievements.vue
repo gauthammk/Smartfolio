a3cd76d2fdf26164d9d46ec2683ae8515b3dce3d
gauthammk <gr8gautham@gmail.com>
2020-08-20 18:44:14 +0530

    added co-curricular achievements column

--- src/components/Achievements.vue
+++ src/components/Achievements.vue
@@ -7,12 +7,31 @@
         </v-row>
         <v-row>
           <v-container class="content">
-            <v-row v-for="achievement in achievements" v-bind:key="achievement.id">
-              <v-container>
-                <v-row class="highlight">{{ achievement.title }}</v-row>
-                <v-row class="italicise">{{ achievement.date }}</v-row>
-                <v-row>{{ achievement.organisation }}</v-row>
-              </v-container>
+            <v-row>
+              <v-col>
+                <v-row
+                  v-for="achievement in curricularAchievements"
+                  v-bind:key="achievement.id"
+                >
+                  <v-container>
+                    <v-row class="highlight">{{ achievement.title }}</v-row>
+                    <v-row class="italicise">{{ achievement.date }}</v-row>
+                    <v-row>{{ achievement.organisation }}</v-row>
+                  </v-container>
+                </v-row>
+              </v-col>
+              <v-col>
+                <v-row
+                  v-for="achievement in coCurricularAchievements"
+                  v-bind:key="achievement.id"
+                >
+                  <v-container>
+                    <v-row class="highlight">{{ achievement.title }}</v-row>
+                    <v-row class="italicise">{{ achievement.date }}</v-row>
+                    <v-row>{{ achievement.organisation }}</v-row>
+                  </v-container>
+                </v-row>
+              </v-col>
             </v-row>
           </v-container>
         </v-row>
@@ -22,14 +41,17 @@
 </template>
 
 <script>
-import achivementsJson from "../content/achievements.json";
+import {
+  curricularAchievements,
+  coCurricularAchievements,
+} from "../content/achievements.json";
 export default {
   name: "Achivements",
   data: () => ({
-    achievements: achivementsJson,
+    curricularAchievements: curricularAchievements,
+    coCurricularAchievements: coCurricularAchievements,
   }),
 };
 </script>
 
-<style>
-</style>
+<style></style>
